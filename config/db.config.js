0b811dc0410aaa93c86bfc1862c769f16216fa2d
Anuj Sharma <19ucc109@lnmiit.ac.in>
2020-12-17 18:31:35 +0530

    config git ignore

--- config/db.config.js
+++ config/db.config.js
@@ -4,13 +4,20 @@ require("firebase/database")
 //Firebase Config here
 const firebaseConfig = {
     // ...
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    // apiKey: "",
+    // authDomain: "",
+    // databaseURL: "",
+    // projectId: "",
+    // storageBucket: "",
+    // messagingSenderId: "",
+    // appId: ""
+    apiKey: "AIzaSyCvesg_oEQSOka1gj_LiDS8m9J9lZWh6-k",
+    authDomain: "simora.firebaseapp.com",
+    databaseURL: "https://simora.firebaseio.com",
+    projectId: "simora",
+    storageBucket: "simora.appspot.com",
+    messagingSenderId: "12230464810",
+    appId: "1:12230464810:web:41b4ceb766e4b610d5fa53"
   }
 
 
